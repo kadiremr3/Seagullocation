3f73120ae88e7997610ceafeed262f45a4f28870
Kadir Emre Yıldırım <kadiremre@icloud.com>
2025-03-07 18:45:46 +0300

    Initial Commit

---
+++ Seagullocation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seagullocation
+//
+//  Created by Kadir Emre Yıldırım on 7.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
